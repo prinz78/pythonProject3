15281eeb35727cedab27ee76ea859914d634f5f4
mouloud.hammoutene <mouloud.hammoutene@uha.fr>
2021-11-18 17:53:06 +0100

    first commit to deploy News3

---
+++ news3/articles/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import (CreateView,
+                                       DeleteView,
+                                       UpdateView,
+                                       )
+from django.urls import reverse_lazy
+from .models import Article
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+# Create your views here.
+
+class ArticleListView(LoginRequiredMixin,ListView):
+    model = Article
+    template_name = 'article_list.html'
+
+class ArticleUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
+    model = Article
+    template_name = 'article_edit.html'
+    fields = ('title', 'body',)
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.auhtor == self.request.user
+
+class ArticleDetailView(LoginRequiredMixin, DetailView):
+    model = Article
+    template_name = 'article_detail.html'
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Article
+    success_url = reverse_lazy('article_list')
+    template_name = 'article_delete.html'
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.auhtor == self.request.user
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
+    model = Article
+    fields = ('title','body',)
+    template_name = 'article_create.html'
+
+    def form_valid(self,form):
+        form.instance.auhtor = self.request.user
+        return super().form_valid(form)
